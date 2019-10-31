4bc5bef7c7398c78845c3b7b37739074428f7407
Jean-Pierre Distler <pdistler@pdistler.de>
2019-10-31 20:25:12 +0100

    Initial Commit

---
+++ ToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Jean-Pierre Distler on 31.10.19.
+//  Copyright © 2019 Jean-Pierre Distler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
